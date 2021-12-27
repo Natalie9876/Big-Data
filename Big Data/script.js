b851dab16be856940f092d6a815ad68b075ea06b
Natalie9876 <95695467+Natalie9876@users.noreply.github.com>
2021-12-27 12:02:02 +0100

    Update script.js

--- Big Data/script.js
+++ Big Data/script.js
@@ -4,7 +4,9 @@ function change(){
   let image = document.getElementById('win');
   if(image_tracker=='normal'){
     image.src='assets/Bilder/MagischeMiesmuschelWin.png';
+    image_tracker = 'perl';
   }else{
-    image.src='assets/Bilder/MagischeMiesmuschel.png'
+    image.src='assets/Bilder/MagischeMiesmuschel.png';
+    image_tracker = 'normal'
   }
 }
